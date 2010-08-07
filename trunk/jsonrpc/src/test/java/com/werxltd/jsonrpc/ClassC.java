6c105e219b64ccf3a36fa9fd1d2c72e1361a0e3f
kai5263499 <kai5263499@2fbc3196-f8e6-11de-abf5-6d7912ff0902>
2010-08-07 12:01:06 +0000

    added simple event messaging

--- trunk/jsonrpc/src/test/java/com/werxltd/jsonrpc/ClassC.java
+++ trunk/jsonrpc/src/test/java/com/werxltd/jsonrpc/ClassC.java
@@ -1,54 +1,33 @@
 package com.werxltd.jsonrpc;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.json.JSONObject;
-
 import com.werxltd.jsonrpc.events.JSONRPCEventListener;
 import com.werxltd.jsonrpc.events.JSONRPCMessage;
 import com.werxltd.jsonrpc.events.JSONRPCMessageEvent;
 
-public class Example implements JSONRPCEventListener {
+public class ClassC implements JSONRPCEventListener {
+	public boolean init = false;
+
 	private ServletConfig config;
 	private HttpSession session;
 	private HttpServletRequest request;
 	private HttpServletResponse response;
+	
 	private Response lastresponse;
 	
-	public Example() {
-		System.out.println("Query init");
-	}
-	
-	public static String test() {
-		return "test successful";
+	public ClassC() {
+		init = true;
 	}
 
-	public float add(float a, float b) {
-		return a + b;
+	public String getConfig() {
+		if(config != null) return config.getInitParameter("rpcclasses");
+		return "";
 	}
 
-	public float sub(float a, float b) {
-		return a - b;
-	}
-	
-	public String list(int a, int b, int c) {
-		return "a:"+a+" b:"+b+" c:"+c;
-	}
-	
-	public String echo(String s) {
-		return s;
-	}
-
-	public String echoJson(JSONObject obj) throws UnsupportedEncodingException {
-		return URLEncoder.encode(obj.toString(), "UTF-8");
-	}
-	
 	public Integer sessionCounter() {
 		Integer count = (Integer) session.getAttribute("count");
 		
@@ -62,13 +41,12 @@ public class Example implements JSONRPCEventListener {
 		return (Integer) session.getAttribute("count");
 	}
 	
-	public String getLastResponse() {
-		if(lastresponse != null) return (String) lastresponse.getResult();
-		return "";
+	public String test() {
+		return "test from class C successful";
 	}
 	
-	public String getConfig() {
-		if(config != null) return config.getInitParameter("rpcclasses");
+	public String getLastResponse() {
+		if(lastresponse != null) return (String) lastresponse.getResult();
 		return "";
 	}
 	
